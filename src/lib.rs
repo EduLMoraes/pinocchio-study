ad3cad090e8046484880be31c2ea931fb9ffe0a6
EduLMoraes <17eduardo05@gmail.com>
2025-02-24 11:11:40 -0300

    Deploy-test: testing invoke

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use pinocchio::{
-    account_info::AccountInfo, entrypoint, instruction::{Account, AccountMeta, Instruction}, msg, program::{invoke, invoke_signed, invoke_unchecked}, pubkey::Pubkey, ProgramResult
+    account_info::AccountInfo,
+    entrypoint,
+    instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke,
+    pubkey::Pubkey,
+    ProgramResult,
 };
 
 entrypoint!(process_instruction);
@@ -14,13 +20,10 @@ pub fn process_instruction(
         .map(|acc| AccountMeta::new(acc.key(), acc.is_writable(), acc.is_signer()))
         .collect();
 
-    let accs: Vec<&AccountInfo> = accounts
-        .iter()
-        .map(|acc| acc)
-        .collect();
+    let accs: Vec<&AccountInfo> = accounts.iter().map(|acc| acc).collect();
 
     let sys_program = pinocchio_pubkey::from_str("11111111111111111111111111111111");
-    
+
     let ix = Instruction {
         program_id: &sys_program,
         data: instruction_data,
