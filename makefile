e1ba6a37513fb6358ec5017f067e8a68ddb7ac2d
EduLMoraes <17eduardo05@gmail.com>
2025-02-24 09:16:15 -0300

    Deploy-test: testing with make

---
+++ makefile
@@ -0,0 +1,13 @@
+deploy:
+	git add .
+	git commit -m "$(filter-out $@,$(MAKECMDGOALS))"
+	cargo check
+	cargo fix --lib -p pinocchio_study
+	cargo fmt
+	cargo build-sbf
+	cargo test
+	solana program deploy ./target/deploy/pinocchio_study.so
+	solana program show --programs
+
+down:
+	solana program close DKwwCKYxHE27QnJ7LLWSFdYXE6ZqGVV6hKrRZZtYhexm --bypass-warning
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,18 @@
-use pinocchio::{
+  use pinocchio::{
     account_info::AccountInfo,
-    default_panic_handler,
+    entrypoint,
     msg,
-    no_allocator,
-    program_entrypoint,
     ProgramResult,
     pubkey::Pubkey
-};
+  };
 
-program_entrypoint!(process_instruction);
+  entrypoint!(process_instruction);
 
-pub fn process_instruction(
+  pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
-) -> ProgramResult {
-    msg!("Hello from `no_std` program!");
+  ) -> ProgramResult {
+    msg!("Hello from my program!");
     Ok(())
-}
+  }
