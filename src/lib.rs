1ea08828bdf6bcb384d32edb42e4b0f17e4f0c11
EduLMoraes <17eduardo05@gmail.com>
2025-02-24 11:11:17 -0300

    Deploy-test: testing invoke

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use pinocchio::{account_info::AccountInfo, entrypoint, msg, pubkey::Pubkey, ProgramResult};
+use pinocchio::{
+    account_info::AccountInfo, entrypoint, instruction::{Account, AccountMeta, Instruction}, msg, program::{invoke, invoke_signed, invoke_unchecked}, pubkey::Pubkey, ProgramResult
+};
 
 entrypoint!(process_instruction);
 
@@ -7,6 +9,30 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let acc_meta: Vec<AccountMeta> = accounts
+        .iter()
+        .map(|acc| AccountMeta::new(acc.key(), acc.is_writable(), acc.is_signer()))
+        .collect();
+
+    let accs: Vec<&AccountInfo> = accounts
+        .iter()
+        .map(|acc| acc)
+        .collect();
+
+    let sys_program = pinocchio_pubkey::from_str("11111111111111111111111111111111");
+    
+    let ix = Instruction {
+        program_id: &sys_program,
+        data: instruction_data,
+        accounts: &acc_meta,
+    };
+
+    invoke(&ix, &[accs.iter().next().unwrap()])?;
+
+    while let Some(account) = accounts.iter().next() {
+        msg!("Conta encontrada...");
+    }
+
     msg!("Hello from my program!");
     Ok(())
 }
